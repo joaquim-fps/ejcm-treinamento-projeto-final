82338190f91ed31edaeb76fba59851689c98627b
Joaquim <joaquim.fps95@gmail.com>
2015-01-26 21:15:34 -0200

    Alterações no arquivo text-trim.js

--- public/js/text-trim.js
+++ public/js/text-trim.js
@@ -3,21 +3,23 @@
     var ultimas = $(".ultimas h4");
     var titulos = $(".noticia-header")
 
+    var limite = $(location).attr('href').indexOf("listar") > -1 ? 350 : 140;
+
     textos.each(function() {
-        if ($(this).text().length > 140) {
-            $(this).text($(this).text().substr(0,137)+"...");
+        if ($(this).text().length > limite) {
+            $(this).text($(this).text().substr(0,limite - 3)+"...");
         }
     });
 
     ultimas.each(function() {
-        if ($(this).text().length > 130) {
-            $(this).text($(this).text().substr(0,127)+"...");
+        if ($(this).text().length > limite - 10) {
+            $(this).text($(this).text().substr(0,limite - 13)+"...");
         }
     });
 
     titulos.each(function() {
-        if ($(this).text().length > 70) {
-            $(this).text($(this).text().substr(0,67)+"...");
+        if ($(this).text().length > limite - 70) {
+            $(this).text($(this).text().substr(0, limite - 73)+"...");
         }
     })
 })(jQuery);
